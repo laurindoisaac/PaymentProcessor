aa5f3f11096b4cff805cdf16c6edf00bbab7ecb1
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 00:47:53 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/paymentprocessor/main.go
@@ -0,0 +1,23 @@
+// cmd/paymentprocessor/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "paymentprocessor/internal/paymentprocessor"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := paymentprocessor.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
